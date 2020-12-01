b71a2e906d03e281a2961ee010de49d6d411aedf
JayTheMate <julenius.jesse@gmail.com>
2020-12-01 11:50:03 +0200

    Main and html colors, sidebar width to 223px, header box shadow

--- js/main.js
+++ js/main.js
@@ -7,11 +7,11 @@ openBtn.addEventListener("click", open);
 
 // function to handle navbar opening
 function open() {
-    if (sidenav.style.width == '250px') {
+    if (sidenav.style.width == '223px') {
         sidenav.style.width = '0';
         sidenav.style.opacity = 0;
     } else {
-        sidenav.style.width = "250px";
+        sidenav.style.width = "223px";
         sidenav.style.opacity = 1.5;
     }
 }
