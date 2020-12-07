f6deb182b25dc6d7f6fd5ce8c31e401f45f6da1f
Joonas Hiltunen <joonas.s.jh@gmail.com>
2020-12-07 18:52:02 +0200

    Added http to https redirect

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+'use strict';
+// redirect to https protocol if needed
+if (window.location.protocol == 'http:') {
+    console.log("Redirect to https protocol");
+    window.location.href =
+    window.location.href.replace( 'http:', 'https:');
+}
+
 // get close and open button elements
 let openBtn = document.getElementById("openbtn");
 let sidenav = document.getElementById("sidenav");
