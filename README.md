31f6d707007c41aa876136e25866e6ec691ce884
LAPTOP-15F6E8QN\Jesse <julenius.jesse@gmail.com>
2020-12-08 11:17:14 +0200

    Weather api added

--- README.md
+++
@@ -1,2 +0,0 @@
-# CampusTracker
-School project
---
+++ js/weather.js
@@ -0,0 +1,64 @@
+'use strict';
+// valitaan elementit
+const temp = document.querySelector(".temperature-value p");
+const notificationElement = document.querySelector(".notification")
+const locationElement = document.querySelector(".location p");
+
+// API:n data säiliöidään tänne: //
+const weather = {};
+
+weather.temperature = {
+	unit : "celsius"
+}
+
+const KELVIN = 273;
+// API avain //
+const key = "85b7cc501fb4434d3bf9fa5868bf702f";
+
+// Tarkistetaan tukeeko käyttäjän nettisivu geolokaatiota //
+if ('geolocation' in navigator) {
+	navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+	notificationElement.style.display = "block";
+	notificationElement.innerHTML = "<p>Selaimesi ei tue Geolokaatiota</p>";
+}
+
+// Haetaan käyttäjän paikka //
+function setPosition(position){
+	let latitude = position.coords.latitude;
+	let longitude = position.coords.longitude;
+	console.log(position.coords.longitude, position.coords.latitude);
+
+	getWeather(latitude, longitude);
+}
+// Jos käyttäjä ei anna paikantamis oikeutta. Tulostetaan virheilmoitus //
+function showError(error) {
+	notificationElement.style.display = "block";
+	notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+// Haetaan säätietio API:n sivulta //
+function getWeather(latitude, longitude) {
+	let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+	fetch(api)
+			.then(function(response){
+				let data = response.json();
+				return data;
+	})
+			.then(function(data){
+				weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+				weather.country = data.sys.country;
+				weather.name = data.name;
+				weather.country = data.sys.country;
+				weather.icon = data.weather[0].icon;
+	})
+			.then(function(){
+				displayWeather();
+	})
+}
+// Tulostetaan Lämpötila näkyviin sivulle DOM:n avulla //
+function displayWeather(){
+	locationElement.innerHTML = `${weather.name}, ${weather.temperature.value}°<span>C</span>  <img src='http://openweathermap.org/img/w/${weather.icon}.png'>`;
+}
+// Tulostetaan konsoliin käyttäjän koordinaatit //
+console.log(navigator.geolocation.getCurrentPosition);
